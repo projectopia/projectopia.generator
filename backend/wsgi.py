07810a99e4fb7fa88b966e09d33d5b7d5ef44616
thangbuiq <buiquangthangtv@gmail.com>
2024-01-06 23:19:39 +0700

    TEST

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,36 +1,31 @@
 from main import app as handler_app
 
-if __name__ == '__main__':
-    handler_app.run()
+import multiprocessing
+import gunicorn.app.base
 
-# import multiprocessing
-# import gunicorn.app.base
+def number_of_workers():
+    _ = multiprocessing.cpu_count()
+    return (4 if _ > 4 else _) * 2 + 1
 
-# def number_of_workers():
-#     _ = multiprocessing.cpu_count()
-#     return (4 if _ > 4 else _) * 2 + 1
+class StandaloneApplication(gunicorn.app.base.BaseApplication):
 
-# class StandaloneApplication(gunicorn.app.base.BaseApplication):
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
 
-#     def __init__(self, app, options=None):
-#         self.options = options or {}
-#         self.application = app
-#         super().__init__()
+    def load_config(self):
+        config = {key: value for key, value in self.options.items()
+                  if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
 
-#     def load_config(self):
-#         config = {key: value for key, value in self.options.items()
-#                   if key in self.cfg.settings and value is not None}
-#         for key, value in config.items():
-#             self.cfg.set(key.lower(), value)
+    def load(self):
+        return self.application
 
-#     def load(self):
-#         return self.application
-
-
-# if __name__ == '__main__':
-#     options = {
-#         'bind': '%s:%s' % ('0.0.0.0', '8080'),
-#         'workers': number_of_workers(),
-#     }
-#     StandaloneApplication(handler_app, options).run()
+options = {
+    'bind': '%s:%s' % ('0.0.0.0', '8080'),
+    'workers': number_of_workers(),
+}
+handler = StandaloneApplication(handler_app, options).run()
 
