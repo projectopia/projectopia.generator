ccb21632a7a2019777d66a6791a4b18c6880e5e0
thangbuiq <buiquangthangtv@gmail.com>
2024-01-06 19:30:09 +0700

    DEPLOY

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,4 +1,31 @@
-from __init__ import app
+import multiprocessing
+import gunicorn.app.base
+from __init__ import app as handler_app
+
+def number_of_workers():
+    _ = multiprocessing.cpu_count()
+    return (4 if _ > 4 else _) * 2 + 1
+
+class StandaloneApplication(gunicorn.app.base.BaseApplication):
+
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        config = {key: value for key, value in self.options.items()
+                  if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
 
 if __name__ == '__main__':
-    app.run()
+    options = {
+        'bind': '%s:%s' % ('0.0.0.0', '8080'),
+        'workers': number_of_workers(),
+    }
+    StandaloneApplication(handler_app, options).run()
